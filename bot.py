3ae91dc28c2640dbce2e9d2c50820183eb72650d
royak47 <52905147+royak47@users.noreply.github.com>
2025-07-15 17:46:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,9 +16,9 @@ from info import *
 from utils import temp
 from Script import script
 from plugins import web_server, check_expired_premium, keep_alive
-from moviestelecast.Bot import moviestelecast
-from moviestelecast.util.keepalive import ping_server
-from moviestelecast.Bot.clients import initialize_clients
+from dreamxbotz.Bot import dreamxbotz
+from dreamxbotz.util.keepalive import ping_server
+from dreamxbotz.Bot.clients import initialize_clients
 
 import logging
 import logging.config
@@ -36,12 +36,12 @@ botStartTime = time.time()
 ppath = "plugins/*.py"
 files = glob.glob(ppath)
 
-async def moviestelecast_start():
+async def dreamxbotz_start():
     print('\n')
-    print('\nInitalizing moviestelecast')
-    await moviestelecast.start()
-    bot_info = await moviestelecast.get_me()
-    moviestelecast.username = bot_info.username
+    print('\nInitalizing DreamxBotz')
+    await dreamxbotz.start()
+    bot_info = await dreamxbotz.get_me()
+    dreamxbotz.username = bot_info.username
     await initialize_clients()
     for name in files:
         with open(name) as a:
@@ -53,7 +53,7 @@ async def moviestelecast_start():
             load = importlib.util.module_from_spec(spec)
             spec.loader.exec_module(load)
             sys.modules["plugins." + plugin_name] = load
-            print("moviestelecast Imported => " + plugin_name)
+            print("DreamxBotz Imported => " + plugin_name)
     if ON_HEROKU:
         asyncio.create_task(ping_server()) 
     b_users, b_chats = await db.get_banned()
@@ -65,13 +65,13 @@ async def moviestelecast_start():
         print("Multiple Database Mode On. Now Files Will Be Save In Second DB If First DB Is Full")
     else:
         print("Single DB Mode On ! Files Will Be Save In First Database")
-    me = await moviestelecast.get_me()
+    me = await dreamxbotz.get_me()
     temp.ME = me.id
     temp.U_NAME = me.username
     temp.B_NAME = me.first_name
     temp.B_LINK = me.mention
-    moviestelecast.username = '@' + me.username
-    moviestelecast.loop.create_task(check_expired_premium(moviestelecast))
+    dreamxbotz.username = '@' + me.username
+    dreamxbotz.loop.create_task(check_expired_premium(dreamxbotz))
     logging.info(f"{me.first_name} with Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
     logging.info(LOG_STR)
     logging.info(script.LOGO)
@@ -79,17 +79,17 @@ async def moviestelecast_start():
     today = date.today()
     now = datetime.now(tz)
     time = now.strftime("%H:%M:%S %p")
-    await moviestelecast.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(temp.B_LINK, today, time))
+    await dreamxbotz.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(temp.B_LINK, today, time))
     app = web.AppRunner(await web_server())
     await app.setup()
     bind_address = "0.0.0.0"
     await web.TCPSite(app, bind_address, PORT).start()
-    moviestelecast.loop.create_task(keep_alive())
+    dreamxbotz.loop.create_task(keep_alive())
     await idle()
     
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     try:
-        loop.run_until_complete(moviestelecast_start())
+        loop.run_until_complete(dreamxbotz_start())
     except KeyboardInterrupt:
         logging.info('Service Stopped Bye 👋')
